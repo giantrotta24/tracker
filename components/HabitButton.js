724a65b5d047647ec11807e8a946455b6c179065
Gian Trotta <giantrotta24@gmail.com>
2021-06-06 14:14:25 -0400

    # 21

--- components/HabitButton.js
+++ components/HabitButton.js
@@ -27,17 +27,43 @@ const REMOVE_EVENT = gql`
   }
 `;
 
-const HabitButton = ({ date }) => {
+const HabitButton = ({ date, habitId, events }) => {
+  // TODO: Update these with optimistic UI
+  const [addEvent] = useMutation(ADD_EVENT, {
+    refetchQueries: ['getHabits'],
+  });
+
+  const [removeEvent] = useMutation(REMOVE_EVENT, {
+    refetchQueries: ['getHabits'],
+  });
+
+  const isFound = false;
+
   // javascript getMonth returns 0-11 😒
   const month = date.getMonth() + 1;
   const monthDate = date.getDate();
 
+  const handleClick = () => {
+    isFound
+      ? removeEvent({
+          variables: {
+            habitId,
+            eventId: 'kjlhgfjkl',
+          },
+        })
+      : addEvent({
+          variables: {
+            habitId,
+            date,
+          },
+        });
+  };
+
+  // TODO: update button X & O with icons (fontawesome?)
   return (
     <span>
       {month}/{monthDate}
-      <button onClick={() => setComplete(!isComplete)}>
-        {isComplete ? 'X' : 'O'}
-      </button>
+      <button onClick={handleClick}>{isFound ? 'X' : 'O'}</button>
       <style jsx>{`
         span {
           display: flex;
